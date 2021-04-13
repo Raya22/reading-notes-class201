494a46a513674cdc2e8d90e9b3fbe31cbe6693ae
Raya22 <raya.lobany94@gmail.com>
2021-04-13 21:09:45 +0300

    add game

--- aboutme/README.md
+++ aboutme/README.md
@@ -1,1 +1,20 @@
-# reading-notes-class201
+# ABOUTME Project
+This Reading notes include "aboutme" project
+which is a page with information about the owner "me"
+with a game questions between me and the user
+
+the project folder contains:
+
+|folder | file-url |
+| ---   | ---- |
+| [css](https://github.com/Raya22/reading-notes-class201/tree/main/aboutme/css)      |   [aboutme.css](https://raya22.github.io/reading-notes-class201/aboutme/css/aboutme.css)   |
+| [img](https://github.com/Raya22/reading-notes-class201/tree/main/aboutme/img)      | [img](https://raya22.github.io/reading-notes-class201/aboutme/img/235-2355120_this-is-me-me.png)      |
+| [js](https://github.com/Raya22/reading-notes-class201/tree/main/aboutme/js)      |[me.js](https://github.com/Raya22/reading-notes-class201/tree/main/aboutme/js)      |
+|       | [.eslintrc.json](https://github.com/Raya22/reading-notes-class201/blob/main/aboutme/.eslintrc.json)     |
+|       | [.gitignore](https://github.com/Raya22/reading-notes-class201/blob/main/aboutme/.gitignore)     |
+|       | [LICENSE](https://raya22.github.io/reading-notes-class201/aboutme/LICENSE)     |
+|       |   [README.md](https://github.com/Raya22/reading-notes-class201/blob/main/aboutme/README.md)   |
+|       | [me.html](https://raya22.github.io/reading-notes-class201/aboutme/me.html)     |
+|       |  [summury.md](https://raya22.github.io/reading-notes-class201/aboutme/summury)    |
+|       |      |
+
--- aboutme/js/me.js
+++ aboutme/js/me.js
@@ -1,39 +1,52 @@
 'use strict';
 
-let user=prompt("what is your name ?");
-alert("welcome "+user+" nice to have you here ");
-console.log("welcome "+user+" nice to have you here ");
 
-let gender = prompt('Before you enter , can you guess my gender (male/female)?\n type one of them');
-console.log("'Before you enter , can you guess my gender (male/female)?\n type one of them'");
+let counter=0;
+
+let user=prompt("what is your name ?");
+alert("welcome "+user+" nice to have you here in the page ");
+//console.log( user);
+
+
+
+let gender = prompt('Before you enter , can you guess my gender (male/female)?\n type one of them').toLowerCase();
+//console.log("'Before you enter , can you guess my gender (male/female)?\n type one of them'");
 switch(gender)
 {
-    case "male"||"Male"||"MALE" :
+    case "male" :
+    case "m" :
         alert("No , I am a female");
         break;
 
-        case "female"||"Female"||"FEMALE" :
+        case "female" :
+        case "f" :
             alert("yes , that's right and my name is Raya");
+            counter++;
             break;
 
             default:
                 alert("you input something wrong");
 }
-console.log(gender);
+//console.log(gender);
 
-let nation =prompt("do you think that I am arabian ?");
-console.log("do you think that I am arabian ?");
+
+
+let nation =prompt("do you think that I am arabian ?").toLowerCase();
+//console.log("do you think that I am arabian ?");
 
 switch(nation)
 {
-    case "yes"||"Yes"||"Yes" :
+    case "yes" :
+        case "y" :
         alert("that's right , I am from Jordan");
-        console.log("that's right , I am from Jordan");
+        counter++;
+        
         break;
 
-        case "no"||"NO"||"No" :
+        case "no" :
+            case "n" :
             alert("no , I am Arabian");
-            console("no , I am Arabian");
+            
             break;
 
             default :
@@ -41,18 +54,100 @@ switch(nation)
             prompt("do you think that I am arabian ?");
 }
 
-/*let study=prompt("do you think I am a student ?");
-if (studt ==="yes"||"Yes"||"Yes") {
+
+let study=prompt("do you think I am a student ?").toLowerCase();
+
+if (study === "yes"|| study==="y") {
     alert("no , I have Graduated two years ago ");
     
-} else if ("no"||"NO"||"No") {
+} else if (study ==="no"|| study ==="n") {
+ alert("That's right, I have Graduated two years ago");
+ counter++;
 
-    alert("That's right, I have Graduated two years ago ");
 } else {
     alert("I think you typed something wrong try to answer with (yes or no)");   
 }
 
-let conf= confirm("do you like ");*/
+
+
+
+let majour= prompt("do you think that I have an experience with IT works ?").toLowerCase();
+if (majour === "yes"|| majour==="y") {
+    alert("That's right , I am a computer engineer ");
+    counter++;
+    
+} else if (majour ==="no"|| majour ==="n") {
+ alert("no ,I am a computer engineer");
+
+} else {
+    alert("I think you typed something wrong try to answer with (yes or no)");   
+}
+
+
+
+
+let travel=prompt("do you think that I love travel ?").toLowerCase();
+
+if (travel === "yes"|| travel==="y") {
+    alert("yes , I love it so much ");
+    counter++;
+    
+} else if (travel ==="no"|| travel ==="n") {
+ alert("No, I love travel so much ");
+ 
+
+} else {
+    alert("I think you typed something wrong try to answer with (yes or no)");   
+}
+
+
+
+
+
+
+for(let i=0;i<4;i++)
+{
+    let num1=prompt("I have a number in my mind , try to guess and type what is the number");
+    if(num1 == 2){
+        alert("yeess that's right");
+        counter++;
+        break;
+    }
+
+    if(num1 > 2)
+    {
+        alert("it's too high");
+    }
+    else{
+        alert("it's too low");
+    }
+    
+
+}
+
+
+
+let travelarray = ["Qatar", "Lebanon", "Syria" , "Kuwait" , "Oman"];
+console.log(travelarray);
+
+    let arrchoice=prompt("try to guess where I traveled before ").toLowerCase();
+
+    while (arrchoice !== 'qatar' && arrchoice !== 'lebanon' && arrchoice !== 'syria' && arrchoice !== 'kuwait' && arrchoice !== 'oman')
+    {
+        for(let k=0 ; k<6 ;k++){
+
+            arrchoice=prompt("try again to guess where I traveled before ").toLowerCase();
+            break;
+
+        }
+
+    }
+
+
+
+
+alert("you answered "+counter+" correct answer out of 7");
+
 
 let conf = confirm("do you want to see the page ?");
 if (conf) {
@@ -62,6 +157,7 @@ if (conf) {
     alert("it is ok , maybe another time ");
     
 }
+
 
 
 
